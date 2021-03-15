b9d6260b7b9f3b6c505b2c5df45873766192f8aa
Captain1045 <amalnc055@gmail.com>
2021-03-15 11:29:55 +0530

    commit on mar 15

--- Angular/bankApp/src/app/app.module.ts
+++ Angular/bankApp/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
